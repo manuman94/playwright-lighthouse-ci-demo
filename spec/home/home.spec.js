2743d636e56174abc8fa6b1b9856200418357e77
jblasco <jblasco@edicom.es>
2021-12-15 15:46:13 +0100

    First commit

---
+++ spec/home/home.spec.js
@@ -0,0 +1,39 @@
+const { chromium } = require('playwright');
+const { playAudit } = require('../../vendor/playwright-lighthouse')
+
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 60000;
+
+describe('Home Lighthouse report', () => {
+
+    const chromiumDebugPort = 9222;
+    const chromiumOptions = {
+        slowMo: 0,
+        chromiumSandbox: false,
+        args: [`--remote-debugging-port=${chromiumDebugPort}`],
+        headless: true
+    };
+
+    it('should pass home thresholds', async () => {
+        try {
+            const browser = await chromium.launch(chromiumOptions);
+            const context = await browser.newContext();
+            const page = await context.newPage();
+            await page.goto('https://github.com/');
+            const lighthouseConfig = {
+                page: page,
+                port: chromiumDebugPort,
+                thresholds: {
+                    performance: 50,
+                    accessibility: 50,
+                    'best-practices': 50,
+                    seo: 50,
+                }
+            };
+            const result = await playAudit(lighthouseConfig);
+            await browser.close();
+        } catch (err) {
+            throw new Error(err);
+        }
+    });
+
+});
