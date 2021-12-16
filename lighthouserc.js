775a0038c0ceccf30c05bcf620799a7d33f68835
jblasco <jblasco@edicom.es>
2021-12-16 16:20:31 +0100

    Generic lighthouserc.js

--- lighthouserc.js
+++ lighthouserc.js
@@ -2,10 +2,8 @@ module.exports = {
     ci: {
         upload: {
             target: 'lhci',
-            serverBaseUrl: 'http://localhost:8080/',
-            token: '0ad6715d-cd31-405e-b14b-5f0b9faf824f',
-            // serverBaseUrl: 'YOUR-SERVER-BASE-URL',
-            // token: 'YOUR-API-TOKEN',
+            serverBaseUrl: 'YOUR-SERVER-BASE-URL',
+            token: 'YOUR-API-TOKEN',
         },
     },
 };
